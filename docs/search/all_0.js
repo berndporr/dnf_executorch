eceea10f0ab234d209b9f1197f1ba701c5b4587f
Bernd Porr <mail@berndporr.me.uk>
2026-03-11 22:44:52 +0000

    Added docs

---
+++ docs/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['deep_20neuronal_20filter_20dnf_0',['Deep Neuronal Filter (DNF)',['../index.html',1,'']]],
+  ['dnf_1',['Deep Neuronal Filter (DNF)',['../index.html',1,'']]]
+];
---
+++ docs/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "dfn",
+  1: "dfn"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+
