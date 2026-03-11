40783dfb63d212b92a41276f4a930bfc45498aa0
Bernd Porr <mail@berndporr.me.uk>
2026-03-11 10:38:02 +0000

    Added 1st test

---
+++ test/test_load_model.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file test_load_model.cpp
+ * @author Bernd Porr
+ * @brief Constructs the DNF and loads the model
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <chrono>
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include "dnf_executorch.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    DNF_executorch dnf("../dnf_executorch.pte");
+}
