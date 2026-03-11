abcb848ca8a662356b5e3cf99d3d3b87f7e9879d
Bernd Porr <mail@berndporr.me.uk>
2026-03-11 23:54:58 +0000

    Removed junk from the python file and added pte file.

--- ecg_filt_demo/export4demo.py
+++ ecg_filt_demo/export4demo.py
@@ -8,14 +8,8 @@ import export2executorch
 def main() -> None:
     pte_filename = "dnf_executorch.pte" 
     nTaps = 50
-    export2executorch.dnf2executorch(pte_filename,nTaps)
-    from executorch.runtime import Runtime
-    runtime = Runtime.get()
-    method = runtime.load_program(pte_filename).load_method("forward")
-    x = torch.randn(1, nTaps)
-    outputs = method.execute([x, torch.ones(1)])
-    print(outputs)
-
+    nLayers = 1
+    export2executorch.dnf2executorch(pte_filename,nTaps,nLayers)
 
 if __name__ == "__main__":
     main()
