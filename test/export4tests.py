ffa9ce9167e053f2c9cdf55fd5221200ad2a324f
Bernd Porr <mail@berndporr.me.uk>
2026-03-12 19:57:15 +0000

    Also testing with 5 layers.

--- test/export4tests.py
+++ test/export4tests.py
@@ -5,11 +5,18 @@ import torch
 sys.path.append('..')
 import export2executorch
 
-def main() -> None:
-    pte_filename = "dnf_executorch.pte" 
+def oneLayer():
+    pte_filename = "dnf_executorch1.pte" 
+    nTaps = 50
+    nLayers = 1
+    export2executorch.dnf2executorch(pte_filename,nTaps,nLayers)
+
+def fiveLayers():
+    pte_filename = "dnf_executorch5.pte" 
     nTaps = 50
     nLayers = 1
     export2executorch.dnf2executorch(pte_filename,nTaps,nLayers)
 
 if __name__ == "__main__":
-    main()
+    oneLayer()
+    fiveLayers()
--- test/test_filtering.cpp
+++ test/test_filtering.cpp
@@ -9,11 +9,11 @@
 // Testing if the DNF is able to learn to eliminate
 // a cosine signal with a sine signal!
 
-double mu = 0.1;
-
-int main(int, char **)
+void doTest(const char* filename, float failAbove)
 {
-    DNF_executorch dnf("dnf_executorch.pte", mu);
+    double mu = 0.1;
+
+    DNF_executorch dnf(filename, mu);
     dnf.setLearning(true);
 
     for (int i = 0; i < 500; i++)
@@ -22,10 +22,17 @@ int main(int, char **)
         const double ref_noise = sin(2 * M_PI / 20 * i) * 0.1;
         const double output_signal = dnf.filter(input_signal, ref_noise);
         printf("%lf %lf %lf\n", input_signal, ref_noise, output_signal);
-        if ( (i > 450) && (fabs(output_signal) > 0.0005) )
+        if ((i > 450) && (fabs(output_signal) > failAbove))
         {
             fprintf(stderr, "LMS did not cancel the sine wave with the cosine wave.\n");
             throw;
         }
     }
+}
+
+
+int main(int, char **)
+{
+    doTest("dnf_executorch1.pte",0.0005);
+    doTest("dnf_executorch5.pte",0.0005);
 }
